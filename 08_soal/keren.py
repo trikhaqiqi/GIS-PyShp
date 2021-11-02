4edb430b555f0bce31fea83f58ebef0a8d6bc829
ilyassstri <trikhaqiqi@gmail.com>
2021-11-02 09:30:27 +0700

    first commit

---
+++ 08_soal/keren.py
@@ -0,0 +1,13 @@
+import shapefile
+w=shapefile.Writer()
+w.shapeType
+
+w.field("kolom1","C")
+w.field("kolom2","C")
+
+w.record("ngek","satu")
+
+
+w.poly(parts=[[[1,3],[5,3],[1,2],[5,2], [1,3]]],shapeType=shapefile.POLYLINE)
+
+w.save("soal8")
---
+++ 09_soal/keren.py
@@ -0,0 +1,14 @@
+import shapefile
+w=shapefile.Writer()
+w.shapeType
+
+w.field("kolom1","C")
+w.field("kolom2","C")
+
+w.record("ngek","satu")
+w.record("crot","dua")
+
+w.poly(parts=[[[1,3],[5,3], [5,2],[1,2], [1,3]]],shapeType=shapefile.POLYLINE)
+w.poly(parts=[[[1,6],[5,6], [5,9],[1,9], [1,6]]],shapeType=shapefile.POLYLINE)
+
+w.save("soal9")
